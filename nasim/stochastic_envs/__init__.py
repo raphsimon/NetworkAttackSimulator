4c708225bf887572663f786560a446723ef08e55
Raphael Simon <ra.simon1998@gmail.com>
2025-07-30 16:43:17 +0200

    Added FlatActionSpacePadded and MultiTaskNASimEnv

--- nasim/stochastic_envs/__init__.py
+++ nasim/stochastic_envs/__init__.py
@@ -1,1 +1,3 @@
+from nasim.stochastic_envs.actions_padded import FlatActionSpacePadded
 from nasim.stochastic_envs.stoch_env import StochNASimEnv
+from nasim.stochastic_envs.multi_task_env import MultiTaskNASimEnv
